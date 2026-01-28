70dd25d8d5b8fecd4341f42f1323515a17a999c1
fxrebxrn <olegsup35@gmail.com>
2026-01-28 10:00:07 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,30 +1,34 @@
 import sqlite3
-from config import DB_NAME
 
-def get_conn():
+DB_NAME = "bot.db"
+
+
+def get_db():
     return sqlite3.connect(DB_NAME)
 
+
 def init_db():
-    with get_conn() as conn:
-        cur = conn.cursor()
+    db = get_db()
+    cur = db.cursor()
 
-        cur.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            user_id INTEGER PRIMARY KEY,
-            company TEXT,
-            queue TEXT
-        )
-        """)
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,
+        company TEXT,
+        queue TEXT
+    )
+    """)
 
-        cur.execute("""
-        CREATE TABLE IF NOT EXISTS schedules (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            company TEXT,
-            queue TEXT,
-            date TEXT,
-            off_time TEXT,
-            on_time TEXT
-        )
-        """)
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS schedules (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        company TEXT,
+        queue TEXT,
+        date TEXT,
+        off_time TEXT,
+        on_time TEXT
+    )
+    """)
 
-        conn.commit()
+    db.commit()
+    db.close()
